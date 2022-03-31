2b2e19b0dcd8d225687083ada3d3df10d67ca98a
walescko <walescko@gmail.com>
2022-03-31 11:17:12 -0300

    C - review printf

--- Pinheiro-ELPRC/Cap02/exemplo2.10.c
+++ Pinheiro-ELPRC/Cap02/exemplo2.10.c
@@ -2,18 +2,23 @@
  * Livro Elementos de Programação em C
  * Esse programa lê dois valores a e b da função do I grau e calcula a raiz da equação ax+b=0 */
 #include <stdio.h>
+
 int main(void){
+    
     double a,b;
+    
     printf("\nSolução de Equação do I Grau ax + b = 0\n");
     printf("Entre com o valor do coeficiente a da equação: ");
     scanf("%lf", &a);
     printf("Entre com o valor do coeficiente b da equação: ");
     scanf("%lf", &b);
-    printf("Equação: %fx + %f = 0\n", a,b);
+
+    printf("Equação: %.2fx + %.2f = 0\n", a,b);
     if (a == 0.0){
         printf("Não existe raiz.\n");
     } else {
-        printf("Raiz = %f \n", (-b/a));
+        printf("Raiz = %.2f \n", (-b/a));
     }
+    
     return 0;
 }    
