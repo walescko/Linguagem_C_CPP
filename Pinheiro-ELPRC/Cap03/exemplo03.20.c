6daefd31b835d7490ba496942e3b6096b5cec1c3
walescko <walescko@gmail.com>
2022-04-07 09:40:41 -0300

    C - bits alignment

---
+++ Pinheiro-ELPRC/Cap03/exemplo03.20.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void printStart(void){
+    printf("\nWadaJu Software Inc.\n");
+}
+
+void printEnd(void){
+    printf("\nFim do Programa.\n");
+}
+
+int main(void){
+    printStart();
+    int alinha_c = __alignof__(char);
+    int alinha_i = __alignof__(int);
+    int alinha_d = __alignof__(double);
+    printf("%d %d %d\n", alinha_c, alinha_i, alinha_d);
+    printEnd();
+    
+    return 0;
+    
+}
