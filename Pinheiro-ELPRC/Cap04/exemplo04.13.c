baa59755eaf76aa0ccc27d53d225c0667a87f8dd
walescko <walescko@gmail.com>
2022-04-10 23:34:29 -0300

    C - liral type and char extend

---
+++ Pinheiro-ELPRC/Cap04/exemplo04.13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "walescko.h"
+
+int main(void){
+    char charA = 'olas';
+
+    printStart();
+    printf("%c %d\n", charA, charA);
+    printEnd();
+
+    return 0;
+
+}
---
+++ Pinheiro-ELPRC/Cap04/exemplo04.14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "walescko.h"
+
+int main(void){
+    char charA = '\xF7';
+    wchar_t charB = L'\xFFFF';
+
+    printStart();
+    printf("%c %hhd\n", charA, charB);
+    printf("%c %d\n", charB, charB)
+    printEnd();
+
+    return 0;
+
+}
