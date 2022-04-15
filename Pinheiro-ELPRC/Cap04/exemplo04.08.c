7e039933f3ab90597651fcf4d778166bec95d6fb
walescko <walescko@gmail.com>
2022-04-15 10:21:16 -0300

    C - exemple 4.08

--- Pinheiro-ELPRC/Cap04/exemplo04.08.c
+++ Pinheiro-ELPRC/Cap04/exemplo04.08.c
@@ -2,13 +2,12 @@
 #include "walescko.h"
 
 int main(void){
-
-    char char01 = 'a';
-
+    
+    char c = 'a';
+    
     printStart();
-    printf("ASCII CODE:\n");
-    printf("%c in ASCII code is %hhd\n", char01, char01);
+    printf("%c %hhd\n", c,c);
     printEnd();
-
+    
     return 0;
 }
