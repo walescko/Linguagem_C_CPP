c6815b193e2ecb4d25c32143a18b2bfd349fef97
Walescko <walesck@gmail.com>
2022-04-17 11:12:28 -0300

    C - variable in the same scope

---
+++ Pinheiro-ELPRC/Cap05/exemplo05.23.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "walescko.h"
+
+int main(void){
+
+    int p1 = 3, p2 = p1;
+
+    struct{
+        char p1;
+        int p2;
+    } reg;
+
+    scanf("%d\n"m &p2);
+    p2: printf("exemplo %d\n", p2);
+        if (p2 > 10){
+            p2 = p2 -1;
+            goto p2;
+        }
+        reg.p1 = 'a';
+        reg.p2 = p2;
+        /*omissaod e codigo*/
+        return 0;
+}
