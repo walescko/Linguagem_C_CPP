8f54f7cc1329b9f9d97e7278e034075af25af61d
Walescko <walescko@gmail.com>
2022-06-24 07:27:13 -0300

    C - exemplo 7.5

---
+++ Pinheiro-ELPRC/Cap07/exemplo07.05.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//
+// Created by walescko on 24/06/22.
+//
+int main(void){
+
+    int number, value, tax;
+
+    printf("\nEntre com três valores inteiros: ");
+    scanf("%d", &number);
+    scanf("%d", &value);
+    scanf("%d", &tax);
+
+    if(number> value){
+        printf(" 1 ");
+        printf(" 2 ");
+    } else {
+        if(val > tax)
+            printf(" 3 ");
+        printf(" 4 ");
+    }
+    printf(" 5\n");
+
+    return 0;
+}
