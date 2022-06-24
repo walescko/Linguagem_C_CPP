6408b27dd93c0e181e49358b1b4afee114160118
Walescko <walescko@gmail.com>
2022-06-24 15:36:34 -0300

    C - switch fail 01

---
+++ Pinheiro-ELPRC/Cap07/exemplo07.08.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//
+// Created by walescko on 24/06/22.
+//
+int main(void){
+
+    int number;
+
+    printf("\nDigite um valor inteiro: ");
+    scanf("%d", &number);
+
+    switch (2+a) {
+        case 23:
+            printf("primeiro\n");
+        default:
+            printf("nenhum\n");
+        case 5*9/3:
+        case 2:
+            printf("segundo\n");
+        case 4:
+            printf("ultimo\n");
+    }
+
+    return 0;
+}
