65df5d2e25974eb5fd06d0e2b10128b9465a88d0
Walescko <walescko@gmail.com>
2022-06-24 15:12:16 -0300

    C - yesno keys

--- Pinheiro-ELPRC/Cap07/exemplo07.07.c
+++ Pinheiro-ELPRC/Cap07/exemplo07.07.c
@@ -11,22 +11,27 @@ int main(void){
 
     printf("\nOs números são: %d, %d e %d.\n", number01, number02, number03);
 
-    if( number01>number02)
-        if (number02 > number03)
+    if( number01>number02) {
+        if (number02 > number03) {
             printf("%d %d %d", number03, number02, number01);
-        else if (number01 > number03)
-            printf("%d %d %d", number02, number03, number01);
-        else
+        } else {
+            if (number01 > number03) {
+                printf("%d %d %d", number02, number03, number01);
+            }
+        } else {
             printf("%d %d %d", number02, number01, number03);
-    else
-        if(number01>number03)
+        }
+    } else {
+        if (number01 > number03) {
             printf("%d %d %d", number03, number01, number02);
-        else
-            if(number02>number03)
-                printf("%d %d %d", number01, number03, number02);
-            else
-                printf("%d %d %d", number01, number02, number03);
-
+        } else {
+            if (number02 > number03) {
+            }
+            printf("%d %d %d", number01, number03, number02);
+        } else {
+            printf("%d %d %d", number01, number02, number03);
+        }
+    }
     printf("\nFim\n");
 
     return 0;
