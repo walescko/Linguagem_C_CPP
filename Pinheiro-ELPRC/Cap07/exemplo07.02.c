f5ea277659a0297ec051ea5e13e112697c543fe7
Walescko <walescko@gmail.com>
2022-06-24 07:10:50 -0300

    C - exemple 7.2 FAIL

---
+++ Pinheiro-ELPRC/Cap07/exemplo07.02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//
+// Created by walescko on 24/06/22.
+//
+int main(void){
+
+    int number;
+    printf("\nDigite um numero inteiro: ");
+    scanf("%d", &number);
+    if(number > 30)
+        printf("\%d maior que 30.\n", number);
+    printf("%d menou ou igual a 30\n", number);
+
+    return 0;
+}
