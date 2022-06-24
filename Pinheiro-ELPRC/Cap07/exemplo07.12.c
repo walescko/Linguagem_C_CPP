72c2923d60f5b22df2ab52ca37a9473987f2e76e
Walescko <walescko@gmail.com>
2022-06-24 17:15:43 -0300

    C - switch ok 02

---
+++ Pinheiro-ELPRC/Cap07/exemplo07.12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//
+// Created by walescko on 24/06/22.
+//
+int main(void) {
+
+    int number;
+
+    printf("\nDigite um número inteiro: ");
+    scanf("%d", &number);
+
+    switch (number*(number-1)+(number-2)) {
+        case -2:
+            printf("Dobro negativo\n");
+            break;
+        case -1:
+            printf("unidade negativa\n");
+            break;
+        default:
+            printf("valor invalido\n");
+            break;
+
+    }
+
+    return 0;
+}
