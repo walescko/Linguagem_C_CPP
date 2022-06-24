3e60f597d1c9e5c33e5bbc642254dd7978d4129f
Walescko <walescko@gmail.com>
2022-06-24 15:46:04 -0300

    C - switch fail 03

--- Pinheiro-ELPRC/Cap07/exemplo07.10.c
+++ Pinheiro-ELPRC/Cap07/exemplo07.10.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
 //
 // Created by walescko on 24/06/22.
 //
+int main(void){
+    
+    int number;
+
+    printf("\nDigite um número inteiro: ");
+    scanf("%d", &number);
+
+    switch (2+number) {
+        int b = 0;
+        case 23: b=23;
+            printf("primeiro apos %d\n", b);
+        default:
+        case 15:
+        case 2:
+            b = 2;
+            printf("primeiro %d\n", b);
+            printf("segundo apos %d\n", b);
+        case 4:
+            printf("primeiro apos %d\n", b);
+    }
+
+    printf("\nFim\n");
+    return 0;
+}
