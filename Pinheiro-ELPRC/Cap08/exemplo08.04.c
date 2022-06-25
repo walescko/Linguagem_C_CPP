d66d54738e3380ed9d704043508ddf53165faace
Walescko <walescko@gmail.com>
2022-06-25 20:48:08 -0300

    C - while 4 - com erros do livro

--- Pinheiro-ELPRC/Cap08/exemplo08.04.c
+++ Pinheiro-ELPRC/Cap08/exemplo08.04.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
 //
 // Created by walescko on 25/06/22.
 //
+int main(void){
+
+    int num = -1;
+    while ((num<=0) || (num>=230)){
+        printf("Digite 0 < numero < 230: ");
+        scanf("%d", &num);
+    }
+    printf("2 x %d = %d\n", num, num*2);
+    printf("\nfim\n");
+    return 0;
+}
