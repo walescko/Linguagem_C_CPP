eef56d6aa113a7dfe41ff65b34f1b443c8fd5e22
Walescko <walescko@gmail.com>
2022-06-26 17:59:23 -0300

    C for with interaction and loops

--- Pinheiro-ELPRC/Cap08/exemplo08.08.c
+++ Pinheiro-ELPRC/Cap08/exemplo08.08.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdio_ext.h>
 //
 // Created by walescko on 26/06/22.
 //
@@ -6,13 +7,20 @@ int main(void){
 
     int soma = 0;
     int quantidade; //quantide de números a somar
+    char resposta = 's';
 
-    printf("Até que número deseja realizar a soma? ");
-    scanf("%d", &quantidade);
-    for (int i = 0; i<=quantidade; i++){
-        soma += i;
-    }
-    printf("A soma dos primeiros números de 0 a %d é %d\n", quantidade, soma);
+    do {
+        printf("Até que número deseja realizar a soma? ");
+        scanf("%d", &quantidade);
+        __fpurge(stdin);
+        for (int i = 0; i <= quantidade; i++) {
+            soma += i;
+        }
+        printf("A soma dos primeiros números de 0 a %d é %d\n", quantidade, soma);
+        printf("\nPara continuar digite 's': ");
+        scanf("%c", &resposta);
+    } while (resposta == 's');
+    printf("\nPrograma encerrado.\n");
 
     return 0;
 }
