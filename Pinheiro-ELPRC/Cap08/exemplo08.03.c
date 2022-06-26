af93491697a0a93fe2559e80dfd8a47f80ebbbb0
Walescko <walescko@gmail.com>
2022-06-25 23:32:32 -0300

    C - while

--- Pinheiro-ELPRC/Cap08/exemplo08.03.c
+++ Pinheiro-ELPRC/Cap08/exemplo08.03.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
 //
 // Created by walescko on 25/06/22.
 //
+int main(void){
+    int qtd = 1;
+    while(qtd <= 1500){
+        printf("1/%d ", ++qtd);
+    }
+    printf("\nFim\n");
+    return 0;
+}
