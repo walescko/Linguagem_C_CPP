72e7db3100dbd33a67abb3d34203ebc27527d77b
Walescko <walescko@gmail.com>
2022-07-01 08:01:23 -0300

    C - functions and procedures no

---
+++ Pinheiro-ELPRC/Cap09/exemplo09.05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//
+// Created by walescko on 01/07/22.
+//
+int main(void){
+
+    funA(4);
+    int funA(double, int);
+    funA(3.7, 66);
+    funB("exem");
+    return 0;
+}
+void funB(int a){
+    funA('a', 57);
+}
+//Esse programa pode até compilar e rodar, mas tem problemas.
+//São exemplos de como não usar funções na Linguagem C.
