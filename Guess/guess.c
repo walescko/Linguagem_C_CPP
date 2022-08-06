0934452cf67981abc247bc91fa845ad2d4b47683
Walescko <walescko@gmail.com>
2022-08-05 22:34:50 -0300

    c - guess header

---
+++ Guess/guess.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//
+// Created by walescko on 05/08/22.
+//
+int main(){
+
+    printf("\n*******************************");
+    printf("\n      Jogo de Adivinhação      ");
+    printf("\n*******************************\n");
+
+    return 0;
+}
