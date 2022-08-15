ec5732fb23e4b490441804aa157287d75f40cae4
Walescko <walescko@gmail.com>
2022-08-15 07:49:40 -0300

    c - guess file

--- Guess/guess.c
+++ Guess/guess.c
@@ -8,5 +8,23 @@ int main(){
     printf("\n      Jogo de Adivinhação      ");
     printf("\n*******************************\n");
 
+    int secretNumber, readNumber, playGame;
+    secretNumber= 42;
+    playGame = 6;
+
+    printf("Digite um número: ");
+    scanf("%d", &readNumber);
+    printf("O número digitado foi %d", readNumber);
+
+    if (secretNumber == readNumber){
+        printf("Você acertou o número!\n");
+        printf("O número secreto é %d!\n", secretNumber);
+    } else {
+        printf("Você errou o número!\n");
+        printf("Tente Novamente.");
+    }
+
+
+
     return 0;
 }
