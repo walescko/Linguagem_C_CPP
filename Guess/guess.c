b52c90cea46179c7f552de83132bba31a141b350
Walescko <walescko@gmail.com>
2022-08-18 23:11:32 -0300

    c - guess, improvement loop and ifs

--- Guess/guess.c
+++ Guess/guess.c
@@ -12,19 +12,27 @@ int main(){
     secretNumber= 42;
     playGame = 6;
 
-    printf("Digite um número: ");
-    scanf("%d", &readNumber);
-    printf("O número digitado foi %d", readNumber);
+    int i;
+    while (i < playGame) {
+        printf("Digite um número: ");
+        scanf("%d", &readNumber);
+        printf("O número digitado foi %d.\n", readNumber);
 
-    if (secretNumber == readNumber){
-        printf("Você acertou o número!\n");
-        printf("O número secreto é %d!\n", secretNumber);
-    } else {
-        printf("Você errou o número!\n");
-        printf("Tente Novamente.");
+        if (secretNumber == readNumber) {
+            printf("Você acertou o número!\n");
+            printf("O número secreto é %d!\n", secretNumber);
+            break;
+        } else {
+            printf("Você errou o número!\n");
+            printf("Tente Novamente.\n");
+            if (readNumber < secretNumber){
+                printf("Dica: O número é maior.\n");
+            } else {
+                printf("Dica: O número é menor.\n");
+            }
+        }
+        i++;
     }
-
-
 
     return 0;
 }
