1c415f5f4cd45775c49471e78858e92fbfe7bfbe
Walescko <walescko@gmail.com>
2022-08-21 21:52:37 -0300

    c - guess with do...while

--- Guess/guess.c
+++ Guess/guess.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdio_ext.h>
+
 //
 // Created by walescko on 05/08/22.
 //
@@ -9,30 +11,36 @@ int main(){
     printf("\n*******************************\n");
 
     int secretNumber, readNumber, playGame;
+    char escolha = 's';
     secretNumber= 42;
     playGame = 6;
 
-    int i;
-    while (i < playGame) {
-        printf("Digite um número: ");
-        scanf("%d", &readNumber);
-        printf("O número digitado foi %d.\n", readNumber);
+    do {
+        int i;
+        while (i < playGame) {
+            printf("Digite um número: ");
+            scanf("%d", &readNumber);
+            printf("O número digitado foi %d.\n", readNumber);
 
-        if (secretNumber == readNumber) {
-            printf("Você acertou o número!\n");
-            printf("O número secreto é %d!\n", secretNumber);
-            break;
-        } else {
-            printf("Você errou o número!\n");
-            printf("Tente Novamente.\n");
-            if (readNumber < secretNumber){
-                printf("Dica: O número é maior.\n");
+            if (secretNumber == readNumber) {
+                printf("Você acertou o número!\n");
+                printf("O número secreto é %d!\n", secretNumber);
+                break;
             } else {
-                printf("Dica: O número é menor.\n");
+                printf("Você errou o número!\n");
+                printf("Tente Novamente.\n");
+                if (readNumber < secretNumber){
+                    printf("Dica: O número é maior.\n");
+                } else {
+                    printf("Dica: O número é menor.\n");
+                }
             }
+            i++;
         }
-        i++;
-    }
+        __fpurge(stdin);
+        printf("Para continuar digite 's': ");
+        scanf("%c", &escolha);
+    } while(escolha == 's');
 
     return 0;
 }
