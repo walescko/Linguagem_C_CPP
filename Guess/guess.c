4d73c3d96e449b1f465fac1dbacbeb87f6778c25
Walescko <walescko@gmail.com>
2022-08-21 22:13:52 -0300

    c - change while -> for

--- Guess/guess.c
+++ Guess/guess.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdio_ext.h>
+#define PLAY_COUNT 3;
 
 //
 // Created by walescko on 05/08/22.
@@ -10,32 +11,34 @@ int main(){
     printf("\n      Jogo de Adivinhação      ");
     printf("\n*******************************\n");
 
-    int secretNumber, readNumber, playGame;
+    int secretNumber, readNumber;
     char escolha = 's';
     secretNumber= 42;
-    playGame = 6;
+
+
 
     do {
         int i;
-        while (i < playGame) {
+        for(int i = 0; i <= PLAY_COUNT; i++){
+
             printf("Digite um número: ");
             scanf("%d", &readNumber);
             printf("O número digitado foi %d.\n", readNumber);
-
-            if (secretNumber == readNumber) {
+            int correct = readNumber == secretNumber;
+            if (correct) {
                 printf("Você acertou o número!\n");
                 printf("O número secreto é %d!\n", secretNumber);
                 break;
             } else {
                 printf("Você errou o número!\n");
                 printf("Tente Novamente.\n");
-                if (readNumber < secretNumber){
+                int menor = readNumber < secretNumber;
+                if (menor){
                     printf("Dica: O número é maior.\n");
                 } else {
                     printf("Dica: O número é menor.\n");
                 }
             }
-            i++;
         }
         __fpurge(stdin);
         printf("Para continuar digite 's': ");
