cf3781e7de863b50a60252bfc926717122aa1c03
Walescko <walescko@gmail.com>
2022-08-22 22:22:43 -0300

    c - else if - clear code

--- Guess/guess.c
+++ Guess/guess.c
@@ -13,32 +13,34 @@ int main(){
 
     int secretNumber, readNumber;
     char escolha = 's';
+    int repeticoes = PLAY_COUNT;
     secretNumber= 42;
+
 
 
 
     do {
         int i;
-        for(int i = 0; i <= PLAY_COUNT; i++){
-
+        for(int i = 0; i < repeticoes; i++){
+//
             printf("Digite um número: ");
             scanf("%d", &readNumber);
             printf("O número digitado foi %d.\n", readNumber);
             int correct = readNumber == secretNumber;
+            int menor = readNumber < secretNumber;
             if (correct) {
                 printf("Você acertou o número!\n");
                 printf("O número secreto é %d!\n", secretNumber);
                 break;
-            } else {
-                printf("Você errou o número!\n");
-                printf("Tente Novamente.\n");
-                int menor = readNumber < secretNumber;
-                if (menor){
+            } else if (menor) {
+//                printf("Você errou o número!\n");
+//                printf("Tente Novamente.\n");
+//                if (menor){
                     printf("Dica: O número é maior.\n");
                 } else {
                     printf("Dica: O número é menor.\n");
                 }
-            }
+//            }
         }
         __fpurge(stdin);
         printf("Para continuar digite 's': ");
